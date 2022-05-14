83d065b31d8d9d646b9531b2423f8cae025a1adc
huohuoerqu <105258656+huohuoerqu@users.noreply.github.com>
2022-05-14 19:47:33 +0800

    Add files via upload

--- mallcast.js
+++ mallcast.js
@@ -3,28 +3,31 @@ var router = express.Router();
 var Product=require("../model/product");
 var mallUser = require("../model/malluse");
 var mallProduct = require("../model/mallproduct");
-var mallOrder = require("../model/mallorder");
 router.get('/', function(req, res, next) {
     if(req.session.malluserInfo){
       mallUser.find({
         name:req.session.malluserInfo["name"]
       }).then(result1=>{
-        mallOrder.find({
+        mallProduct.find({
             buyer:req.cookies["mallcurrentuser"]
           }).then(result=>{
             console.log(result)
-            res.render('mallorder', { title: '有机农产品购物车页面',name:req.cookies["mallcurrentuser"], isShow:false,info:result[0]});
+            res.render('mallcast', { title: '有机农产品购物车页面',name:req.cookies["mallcurrentuser"], isShow:false,info:result});
           })
       })
     }else{
       res.send(`您还未登录，请<a href="/malllogin">登录</a>`);
     }
 });
-router.get('/orderjudge', function(req, res, next) {
+router.get('/castjudge', function(req, res, next) {
   Product.find({
-            _id:req.query.id  
+            _id:req.query.id
           }).then(presult=>{
-            mallOrder.create({
+            mallProduct.find({
+              id:req.query.id,
+           }).then(mpresult=>{
+          if(mpresult.length==0){
+            mallProduct.create({
                 author:presult[0].author,
                 id:req.query.id,
                 buyer:req.cookies["mallcurrentuser"],
@@ -32,19 +35,30 @@ router.get('/orderjudge', function(req, res, next) {
                 kindtype:presult[0].kindtype,
                 price:presult[0].price,
                 num:req.query.num,
-                total:req.query.total,
                 date:presult[0].date,
                 detail:presult[0].detail,
                 pathname:presult[0].pathname
               })
-          })    
+          }else {
+            var oNum=Number(mpresult[0].num)+Number(req.query.num)
+            mallProduct.update({
+              id:req.query.id,
+            },{$set:{id:req.query.id,proname:presult[0].proname,price:presult[0].price,kindtype:presult[0].kindtype,num:oNum,date:presult[0].date,detail:presult[0].detail,pathname:presult[0].pathname}}).then(result=>{
+            })
+          }
+          res.send({
+         });
+       })
+
+          })
+      
 });
-router.get('/orderlist', function(req, res, next) {
+router.get('/castlist', function(req, res, next) {
   if(req.session.malluserInfo){
     mallUser.find({
       name:req.session.malluserInfo["name"]
     }).then(result1=>{
-      mallOrder.find({
+      mallProduct.find({
           buyer:req.cookies["mallcurrentuser"]
         }).then(result=>{
           res.send({
@@ -57,8 +71,8 @@ router.get('/orderlist', function(req, res, next) {
   }
 });
 router.get("/delete",function(req,res){
-  mallOrder.findByIdAndRemove(req.query.id).then(result=>{
-    res.redirect("/mallorder");
+  mallProduct.findByIdAndRemove(req.query.id).then(result=>{
+    res.redirect("/mallcast");
   })
 })
   module.exports = router;
